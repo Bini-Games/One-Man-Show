7aee853d1116bc78889ffcb6c2fe20c795f7fa03
bilasyurii <bilas.yurii@gmail.com>
2023-04-01 14:26:46 +0300

    World state.

--- src/world/world.ts
+++ src/world/world.ts
@@ -3,6 +3,9 @@ import { Parent } from "./parent";
 import { Target } from "./target";
 
 export class World {
+  public static readonly statesCount: number = 8;
+  public static readonly actionsCount: number = 5; // move left, right, up, down, or stay
+
   public child: Child;
   public parent: Parent;
   public target: Target;
@@ -14,7 +17,10 @@ export class World {
   public reset(): void {
     const child = this.child;
     child.position.set(Math.random(), Math.random());
-    child.velocity.set(Math.random() * 0.05 - 0.025, Math.random() * 0.05 - 0.025);
+    child.velocity.set(
+      Math.random() * 0.05 - 0.025,
+      Math.random() * 0.05 - 0.025
+    );
 
     const parent = this.parent;
     parent.position.set(Math.random(), Math.random());
@@ -22,6 +28,30 @@ export class World {
 
     const target = this.target;
     target.position.set(Math.random(), Math.random());
+  }
+
+  public getState(): number[] {
+    const child = this.child;
+    const target = this.target;
+    const parent = this.parent;
+
+    return [
+      child.position.x - 0.5,
+      child.position.y - 0.5,
+      child.velocity.x * 10,
+      child.velocity.y * 10,
+      target.position.x - child.position.x,
+      target.position.y - child.position.y,
+      parent.position.x - child.position.x,
+      parent.position.y - child.position.y,
+    ];
+  }
+
+  public doAction(action: number): void {
+  }
+
+  public getReward(): number {
+    return 0;
   }
 
   protected init(): void {
@@ -42,4 +72,3 @@ export class World {
     this.target = new Target();
   }
 }
-
