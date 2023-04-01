1292eea5fdbea86c90014110567e4614d33e22f1
bilasyurii <bilas.yurii@gmail.com>
2023-04-02 01:13:58 +0300

    EventBus in game locator.

--- src/core/facade/game.ts
+++ src/core/facade/game.ts
@@ -1,9 +1,14 @@
 import { ServiceLocator } from "../services/service-locator";
+import { EventBus } from "./event-bus";
 import { Logger } from "./logger";
 
 export class GameClass extends ServiceLocator {
   public get logger(): Logger {
     return this.getService(Logger.key);
+  }
+  
+  public get events(): EventBus {
+    return this.getService(EventBus.key);
   }
 }
 
