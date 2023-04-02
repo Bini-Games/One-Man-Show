da50512bebfaafc7a3024a571e4cf4410ed16d1a
bilasyurii <bilas.yurii@gmail.com>
2023-04-02 21:47:31 +0300

    Added starting full screen fail handling.

--- src/core/screen/fullscreen.ts
+++ src/core/screen/fullscreen.ts
@@ -1,15 +1,20 @@
 import { GameConfig } from "../../game/data/game-config";
+import { Game } from "../facade/game";
 
-export function requestFullscreen(): void {
+export async function requestFullscreen(): Promise<void> {
   const documentElement: any = document.getElementById(GameConfig.CanvasId);
 
-  if(documentElement.requestFullscreen) {
-    documentElement.requestFullscreen();
-  } else if(documentElement.mozRequestFullScreen) {
-    documentElement.mozRequestFullScreen();
-  } else if(documentElement.webkitRequestFullscreen) {
-    documentElement.webkitRequestFullscreen();
-  } else if(documentElement.msRequestFullscreen) {
-    documentElement.msRequestFullscreen();
+  try {
+    if(documentElement.requestFullscreen) {
+      await documentElement.requestFullscreen();
+    } else if(documentElement.mozRequestFullScreen) {
+      await documentElement.mozRequestFullScreen();
+    } else if(documentElement.webkitRequestFullscreen) {
+      await documentElement.webkitRequestFullscreen();
+    } else if(documentElement.msRequestFullscreen) {
+      await documentElement.msRequestFullscreen();
+    }
+  } catch (e) {
+    Game.logger.log(`Couldn't start full screen mode`);
   }
 }
